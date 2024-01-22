d82ad82e54ceb0d15aa138dc140d3711a7c2c3b5
thisiskhandev <hassan.khan.dev@proton.me>
2024-01-23 04:31:38 +0500

    Event listeners & Functions

---
+++ JS/functions.js
@@ -0,0 +1,19 @@
+let heading = document.getElementsByTagName("h1")[0];
+console.log(heading);
+// Parameters
+heading.innerHTML = "<ul>";
+
+function sum(param1, param2) {
+  let sum = param1 + param2;
+  console.log(sum);
+  heading.innerHTML += "<li>" + sum + "</li>";
+}
+
+heading.innerHTML += "</ul>";
+
+// sum(2, 4); // Arguments
+sum(5, 5); // Calling
+
+sum("Salman", " Ahmed");
+sum("Hassan", " Khan");
+sum();
